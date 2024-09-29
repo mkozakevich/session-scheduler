8e56007d16be87f7100216fc9013e4076ebd99c6
Maksim Kozakevich <kotseee@yandex.ru>
2024-09-30 00:53:13 +0300

    feat: add dnd

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,12 +1,48 @@
+'use client';
+
 import { Project } from '@/components/project/component';
 import { data } from './data.';
+import { useState } from 'react';
+import { DndContext, DragEndEvent, closestCenter } from '@dnd-kit/core';
+import {
+    SortableContext,
+    arrayMove,
+    verticalListSortingStrategy,
+} from '@dnd-kit/sortable';
 
 export default function Home() {
+    const [projects, setProjects] = useState(data);
+
+    const handleDragEnd = (event: DragEndEvent) => {
+        const { active, over } = event;
+
+        if (active.id !== over?.id) {
+            setProjects((projects) => {
+                const oldIndex = projects.findIndex(
+                    (project) => project.id === active.id
+                );
+                const newIndex = projects.findIndex(
+                    (project) => project.id === over?.id
+                );
+
+                return arrayMove(projects, oldIndex, newIndex);
+            });
+        }
+    };
+
     return (
-        <>
-            {data.map((project) => (
-                <Project key={project.id} project={project} />
-            ))}
-        </>
+        <DndContext
+            onDragEnd={handleDragEnd}
+            collisionDetection={closestCenter}
+        >
+            <SortableContext
+                items={projects}
+                strategy={verticalListSortingStrategy}
+            >
+                {projects.map((project) => (
+                    <Project key={project.id} project={project} />
+                ))}
+            </SortableContext>
+        </DndContext>
     );
 }
--- src/components/project/component.tsx
+++ src/components/project/component.tsx
@@ -1,12 +1,28 @@
 import { IProject } from '@/interfaces/IProject';
 import styles from './styles.module.scss';
 import declineWord from 'decline-word';
+import { useSortable } from '@dnd-kit/sortable';
+import { CSS } from '@dnd-kit/utilities';
 
 export const Project = ({ project }: { project: IProject }) => {
     const { id, name, numberOfParticipants } = project;
 
+    const { attributes, listeners, setNodeRef, transform, transition } =
+        useSortable({ id });
+
+    const style = {
+        transform: CSS.Transform.toString(transform),
+        transition,
+    };
+
     return (
-        <div className={styles.wrapper}>
+        <div
+            className={styles.wrapper}
+            ref={setNodeRef}
+            style={style}
+            {...attributes}
+            {...listeners}
+        >
             <div className={styles.container}>
                 <div>
                     <div className={styles.timing}>10:00 – 10:25</div>
