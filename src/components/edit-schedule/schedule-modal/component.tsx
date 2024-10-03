c8387f6df8c9f88c0b8eb15c8e239b0e18b724cd
Maksim Kozakevich <kotseee@yandex.ru>
2024-10-03 19:50:09 +0300

    feat: add inputs to modal

--- src/components/edit-schedule/schedule-modal/component.tsx
+++ src/components/edit-schedule/schedule-modal/component.tsx
@@ -2,7 +2,9 @@ import styles from './styles.module.scss';
 import classNames from 'classnames/bind';
 import AtomIcon from '@/assets/atom.svg';
 import CrossIcon from '@/assets/cross.svg';
+import { DatePicker, TimePicker } from 'antd';
 
+const { RangePicker } = DatePicker;
 const cx = classNames.bind(styles);
 
 export const ScheduleModal = ({ close }: { close: Function }) => {
@@ -16,7 +18,9 @@ export const ScheduleModal = ({ close }: { close: Function }) => {
                     <div className={styles.subtitle}>
                         <AtomIcon />
                         <div className={styles.comission}>
-                            <div className={styles.comissionNumber}>Комиссия №1</div>
+                            <div className={styles.comissionNumber}>
+                                Комиссия №1
+                            </div>
                             <div className={styles.comissionName}>НИР</div>
                         </div>
                     </div>
@@ -29,7 +33,24 @@ export const ScheduleModal = ({ close }: { close: Function }) => {
                     />
                 </div>
             </div>
-            <div className={styles.content}></div>
+
+            <div className={styles.content}>
+                <div className={styles.inputContainer}>
+                    <div>Даты</div>
+                    <RangePicker />
+                </div>
+                <div className={styles.timeContainer}>
+                    <div className={styles.inputContainer}>
+                        <div>C</div>
+                        <TimePicker />
+                    </div>
+                    <div className={styles.inputContainer}>
+                        <div>До</div>
+                        <TimePicker />
+                    </div>
+                </div>
+            </div>
+
             <div className={styles.footer}>
                 <button
                     className={cx(styles.button, styles.discardButton)}
