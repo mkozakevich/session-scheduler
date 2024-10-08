9f97c99e443082278ca478c170b1f2ef76b8b034
Maksim Kozakevich <kotseee@yandex.ru>
2024-10-09 00:40:43 +0300

    fix: add comment to doc

--- src/functions/calculateEndDate.ts
+++ src/functions/calculateEndDate.ts
@@ -1,5 +1,11 @@
 import { DateArg, addMinutes } from 'date-fns';
 
+/**
+ * Вычисляет дату окончания проекта
+ * @param startDate - Дата начала проекта
+ * @param numberOfParticipants - Число участников
+ * @returns 
+ */
 export const calculateEndDate = (
     startDate: DateArg<Date>,
     numberOfParticipants: number
